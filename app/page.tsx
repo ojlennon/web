07c1db95189b7f7f4d4a1cadb856622c38439fff
ojlennon <45679125+ojlennon@users.noreply.github.com>
2024-11-07 20:28:31 -0500

    made top bar horizontal

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,58 @@
-'use client'
+"use client";
 
 import { useRouter } from "next/navigation";
-import Button from '../components/Button'
+import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
+
   const handleClick = () => {
-  router.push('/contact')
-}
-  return (
+    router.push("/contact");
+  };
+
+  return (    
     <div>
-      <nav>
-          <ul>
-              <img id="logo" src="../public/images/michigan.png"></img>
-              <li><a href="">About</a></li>
-              <li><a href="">Projects</a></li>
-              <li><a href="">Login</a></li>
-              <li><a href="">Sign Up</a></li>
-          </ul>
-      </nav>
+      <div style={{
+        display: 'flex',
+        gap: '10px'
+      }}>
+        <Image
+            src="/michigan.png"
+            alt="Picture of the author"
+            width={50}
+            height={50}
+            quality={100}
+          />
+          <p>
+            <a href="">About</a>
+          </p>
+          <p>
+            <a href="">Projects</a>
+          </p>
+          <p>
+            <a href="">Login</a>
+          </p>
+          <p>
+            <a href="">Sign Up</a>
+          </p>
+      </div>
 
       <div id="title">
-      <h1> FindBlue.</h1>
+        <h1> FindBlue.</h1>
       </div>
+
       <div id="aboutUs">
-      <h1 id ="aboutTitle"> About</h1>
-      <p>Our idea is not unique. Well, only if you think broadly. If you are looking for qualified people, 
-        you use LinkedIn. If you want to find a co-founder for a startup, there are tons of platforms which you can do so.
-         It is so easy that if you simply search “find a cofounder” in google, you get a page full of platforms that provide a similar service. 
-         If we try to infiltrate this market, we’ll drown in competition and constantly struggle for time in the spotlight. 
-         However, if you think more narrowly, our idea has the potential to have a monopoly on a giant market. 
-         This market is aspiring college students who are dying to find projects and teams. 
-         If we can get our project to be the number one platform that University of Michigan students use to start up an idea, 
-         we’ll have a monopoly on THE public university in North America.</p>
+        <h1 id="aboutTitle">About</h1>
+        <p>
+          Our idea is not unique. Well, only if you think broadly. If you are
+          looking for qualified people, you use LinkedIn...
+        </p>
       </div>
+
       <div id="footnote">
-      <a href="">FAQ</a>
-      <a href="">Contact Us</a>
-      <a href="">Founders</a>
+        <a href="">FAQ</a>
+        <a href="">Contact Us</a>
+        <a href="">Founders</a>
       </div>
     </div>
   );
